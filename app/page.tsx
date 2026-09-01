6aecb9b290b18f815f1d5666709943869a7fe909
phe0nix <starlordindia1230@gmail.com>
2026-09-01 13:05:00 +0530

    first push

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-1 w-full max-w-3xl ">
+      <h1 className="text-3xl font-semibold">Welcome to Next.js!</h1>
+    </main>
+  );
+}
