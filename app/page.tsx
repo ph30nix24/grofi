497f849f5fa08564092b8d14cc09d467cf3d8a9e
phe0nix <starlordindia1230@gmail.com>
2026-09-01 13:09:35 +0530

    new fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 w-full max-w-3xl ">
+    <main className="flex flex-1 w-full max-w-7xl mx-auto">
       <h1 className="text-3xl font-semibold">Welcome to Next.js!</h1>
     </main>
   );
